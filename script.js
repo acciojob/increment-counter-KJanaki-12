d3cff7304eccc5e5afc66f5a17881723bff35bbd
KJanaki-12 <kenchugundujanaki@gmail.com>
2026-07-31 16:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+//your JS code here. If rhttps://www.svgrepo.com/show/345221/three-dots.svgequired.
+let counter = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+
+count = parseInt(counter.innerText);
+function increseValue() {
+	alert(count);
+	count++;
+	counter.innerText = `${count}`;
+}
+
+btn.addEventListener("click", increseValue);
+
